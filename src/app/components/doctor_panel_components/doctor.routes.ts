163285c90fd63a86c3b90aef9a4f828869465a89
faraz78691 <mohdfaraz.github@gmail.com>
2025-05-16 16:08:01 +0530

    started doc panel

--- src/app/components/doctor_panel_components/doctor.routes.ts
+++ src/app/components/doctor_panel_components/doctor.routes.ts
@@ -4,12 +4,21 @@ import { AuthGuard } from '../../guards/auth.guard';
 
 export const doctorRoutes: Routes = [
       {
+            path: 'profile-setup',
+            canActivate: [],
+            // data: { roles: ['clinic'] },
+            loadComponent: () => import('../doctor_panel_components/profile-setup/profile-setup.component').then(m => m.ProfileSetupComponent)
+      },
+      {
             path: '',
             canActivate: [AuthGuard],
-            data: { roles: ['doctor'] },
-            //     children: [
-            //       { path: '', component: DashboardComponent },
-            //       { path: 'patients', loadComponent: () => import('./patients.component').then(m => m.PatientsComponent) }
-            //     ]
+            data: { roles: ['clinic'] },
+            loadComponent: () => import('../main/main.component').then(m => m.MainComponent),
+            children: [
+                  {
+                        path: '',
+                        loadComponent: () => import('../clinic_panel_components/dashboard/dashboard.component').then(m => m.DashboardComponent)
+                  },
+            ]
       }
 ];
