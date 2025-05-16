04ff396bd273a4e34c04ff0b802ea071f3116492
faraz78691 <mohdfaraz.github@gmail.com>
2025-05-16 16:40:04 +0530

    added models

--- src/app/components/auth/forget-password/forget-password.component.ts
+++ src/app/components/auth/forget-password/forget-password.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.css'
 })
 export class ForgetPasswordComponent {
+  forgotPasswordForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  get email() {
+    return this.forgotPasswordForm.get('email');
+  }
+
+  onSubmit() {
+    if (this.forgotPasswordForm.valid) {
+      const formData = this.forgotPasswordForm.value;
+ 
+     
+    } else {
+      this.forgotPasswordForm.markAllAsTouched(); // shows validation errors
+    }
+  }
 }
